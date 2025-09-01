71bc78d51e329bc7a809139cdcda4db759b48a93
laizu01993 <laizuaktar6@gmail.com>
2025-09-02 01:38:10 +0600

    add post /blog api to insert new blog from client site to mongodb

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // middlewear
 app.use(cors());
 app.use(express.json());
 
 // connect with mongodb
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.tye2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tye2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +26,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // collection
+        const blogCollection = client.db('blogDB').collection('blog');
+
+        // for getting data from add blog form in the client side
+        app.post('/blog', async(req, res) =>{
+            const newBlog = req.body;
+            const result = await blogCollection.insertOne(newBlog);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
